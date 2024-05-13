b7f75acc83e48f852bf34641bdb51385b6b9deea
Alex Iraola <alex.iraola@gmail.com>
2024-05-14 00:50:29 +0200

    More test cases

--- kata-bowling/src/lib.rs
+++ kata-bowling/src/lib.rs
@@ -11,7 +11,30 @@ impl BowlingGame {
     }
 
     pub fn calculate_result(&self) -> i32 {
-        return self.rolls.iter().sum::<i32>();
+        let mut frame_index = 0;
+        let mut points = 0;
+
+        for _ in 0..10 {
+            if self.is_strike(frame_index) {
+                points += 10 + self.rolls[frame_index + 1] + self.rolls[frame_index + 2];
+                frame_index += 1;
+            } else if self.is_spare(frame_index) {
+                points += 10 + self.rolls[frame_index + 2];
+                frame_index += 2;
+            } else {
+                points += self.rolls[frame_index] + self.rolls[frame_index + 1];
+                frame_index += 2;
+            }
+        }
+        points
+    }
+
+    fn is_spare(&self, frame_index: usize) -> bool {
+        self.rolls[frame_index] + self.rolls[frame_index + 1] == 10
+    }
+
+    fn is_strike(&self, frame_index: usize) -> bool {
+        self.rolls[frame_index] == 10
     }
 }
 
@@ -32,6 +55,15 @@ mod tests {
     }
 
     #[test]
+    fn zero_hits_get_zero_points() {
+        let mut bowling_game = BowlingGame::new();
+
+        roll_many(&mut bowling_game, 20, 0);
+
+        assert_eq!(bowling_game.calculate_result(), 0);
+    }
+
+    #[test]
     fn all_ones_sums_twenty() {
         let mut bowling_game = BowlingGame::new();
 
@@ -47,5 +79,40 @@ mod tests {
         roll_many(&mut bowling_game, 20, 2);
 
         assert_eq!(bowling_game.calculate_result(), 40);
+    }
+
+    #[test]
+    fn calculates_score_for_a_given_spare_extra_ball() {
+        let mut bowling_game = BowlingGame::new();
+
+        bowling_game.roll(5);
+        bowling_game.roll(5);
+        bowling_game.roll(5);
+
+        roll_many(&mut bowling_game, 17, 0);
+
+        assert_eq!(bowling_game.calculate_result(), 20);
+    }
+
+    #[test]
+    fn calculates_score_for_a_given_strike_extra_ball() {
+        let mut bowling_game = BowlingGame::new();
+
+        bowling_game.roll(10);
+        bowling_game.roll(2);
+        bowling_game.roll(3);
+
+        roll_many(&mut bowling_game, 16, 0);
+
+        assert_eq!(bowling_game.calculate_result(), 20);
+    }
+
+    #[test]
+    fn calculates_score_for_a_perfect_game() {
+        let mut bowling_game = BowlingGame::new();
+
+        roll_many(&mut bowling_game, 12, 10);
+
+        assert_eq!(bowling_game.calculate_result(), 300);
     }
 }
