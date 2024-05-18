84629d576e5c8f207f4ed8e777ceb217c3f56d5d
Alex Iraola <alex.iraola@gmail.com>
2024-05-18 23:42:28 +0200

    Complete minigrep

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,3 +1,4 @@
+use minigrep::Config;
 use std::process;
 
 fn main() {
@@ -8,32 +9,8 @@ fn main() {
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
-
-    run(config);
-}
-
-fn run(config: Config) {
-    let contents =
-        std::fs::read_to_string(config.file_path).expect("Should have been able to read the file");
-
-    println!("With text:\\n{contents}");
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
     }
 }
