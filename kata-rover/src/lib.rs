6454a9570a442bdd356a6372e1e8a15d9e3d35fc
Alex Iraola <alex.iraola@gmail.com>
2024-05-21 19:47:11 +0200

    Turns right and left

--- kata-rover/src/lib.rs
+++ kata-rover/src/lib.rs
@@ -7,15 +7,42 @@
 // LLFF => '0:8:S'
 // FRFFR => '2:1:S'
 
+#[derive(Debug)]
+enum Orientation {
+    N,
+    E,
+    S,
+    W,
+}
+
 pub struct Rover {
     x: i32,
     y: i32,
-    orientation: String,
+    orientation: Orientation,
 }
 
 impl Rover {
     pub fn position(&self) -> String {
-        String::from("0:0:N")
+        format!("{}:{}:{:#?}", self.x, self.y, self.orientation)
+    }
+
+    pub fn execute(&mut self, commands: Vec<String>) {
+        if commands[0].eq("L") {
+            self.orientation = Orientation::W;
+        }
+        if commands[0].eq("R") {
+            self.orientation = Orientation::E;
+        }
+    }
+}
+
+impl Default for Rover {
+    fn default() -> Self {
+        Self {
+            x: 0,
+            y: 0,
+            orientation: Orientation::N,
+        }
     }
 }
 
@@ -25,11 +52,25 @@ mod tests {
 
     #[test]
     fn creates_rover_at_initial_position() {
-        let rover = Rover {
-            x: 0,
-            y: 0,
-            orientation: String::from("N"),
-        };
+        let rover = Rover::default();
         assert_eq!(rover.position(), "0:0:N");
+    }
+
+    #[test]
+    fn turns_left() {
+        let mut rover = Rover::default();
+
+        rover.execute(vec!["L".to_string()]);
+
+        assert_eq!(rover.position(), "0:0:W");
+    }
+
+    #[test]
+    fn turns_right() {
+        let mut rover = Rover::default();
+
+        rover.execute(vec!["R".to_string()]);
+
+        assert_eq!(rover.position(), "0:0:E");
     }
 }
