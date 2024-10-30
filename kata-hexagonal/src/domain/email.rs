9f62dfb37f6bbbd765e19b30e6adeb5fd18ce0bc
Alex Iraola <alex.iraola@gmail.com>
2024-10-30 15:42:05 +0100

    Finish email value object

--- kata-hexagonal/src/domain/email.rs
+++ kata-hexagonal/src/domain/email.rs
@@ -1,26 +1,39 @@
 use core::panic;
+use std::fmt;
 
 use regex::Regex;
 
+#[derive(Debug)]
 pub struct Email {
     email: String,
 }
 
 impl Email {
-    pub fn new(email: &str) -> Self {
-        let email_regex = Regex::new(r"^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$").unwrap();
-        if !email_regex.is_match(email) {
-            panic!("Invalid email format");
-        }
-        Self {
-            email: String::from(email),
-        }
+    pub fn create(address: String) -> Self {
+        Self::ensure_is_valid_email(&address);
+        Self { email: address }
     }
 
-    pub fn to_string(&self) -> String {
-        self.email.clone()
+    fn ensure_is_valid_email(address: &str) {
+        let email_regex = Regex::new(r"^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$").unwrap();
+        if !email_regex.is_match(address) {
+            panic!("Invalid email format");
+        }
     }
 }
+
+impl fmt::Display for Email {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.email)
+    }
+}
+
+impl PartialEq for Email {
+    fn eq(&self, other: &Self) -> bool {
+        self.email == other.email
+    }
+}
+impl Eq for Email {}
 
 #[cfg(test)]
 mod test {
@@ -28,14 +41,30 @@ mod test {
 
     #[test]
     fn create_email_with_correct_format() {
-        let address = "example@example.com";
-        let email = Email::new(address);
-        assert_eq!(email.to_string(), address);
+        let address = String::from("example@example.com");
+        let email = Email::create(address);
+        assert_eq!(email.to_string(), String::from("example@example.com"));
     }
 
     #[test]
     #[should_panic(expected = "Invalid email format")]
     fn fails_creating_with_invalid_format() {
-        Email::new("invalid");
+        Email::create("invalid".to_string());
+    }
+
+    #[test]
+    fn two_emails_with_same_address_should_be_equal() {
+        assert_eq!(
+            Email::create("test@example.com".to_string()),
+            Email::create("test@example.com".to_string())
+        );
+    }
+
+    #[test]
+    fn two_emails_with_different_address_should_not_be_equal() {
+        assert_ne!(
+            Email::create("tes@example.com".to_string()),
+            Email::create("test@example.com".to_string())
+        );
     }
 }
